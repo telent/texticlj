608ce1b1ff3a95013c0bbd8e9b0a31bb089a14e7
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2015-02-11 08:51:03 +0000

    boilerplate

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject texticlj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Convert approximately-Textile markup to Hiccup"
+  :url "http://github.com/telent/texticlj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
